3dcfe0e70e92627ab66c40a377d938169e1d5bb7
daniel <57467819+DanielCW7@users.noreply.github.com>
2023-03-30 19:31:19 -0500

    Update stats.js

--- src/components/stats.js
+++ src/components/stats.js
@@ -3,7 +3,8 @@ import React from "react";
 const Stats = () => {
     return (
         <div className="p-5 md:p-10 flex flex-col justify-content">
-            <div className="flex flex-row justify-evenly gap-20 border">
+
+            <div className="flex flex-row justify-evenly gap-20 my-10">
                 <div className="flex flex-col text-center">
                     <h2 className="text-xl"> Owned </h2>
                     <span className="text-5xl lg:text-7xl font-black text-green-500"> 250 </span>
@@ -17,7 +18,12 @@ const Stats = () => {
                     <span className="text-5xl lg:text-7xl font-black text-green-500"> 50 </span>
                 </div>           
             </div>
-
+            <div>
+                {/* this should illustrate the proportion of comics by collection */}
+                <p>chart.js and legend of comic owned [here]</p>
+            </div>
+            {/* need actual "profile" capabilities: changing PFP, settings, darkmode, preferences(?)
+                change backgrounds(?), saved items(>.> ?) */}
         </div>
     )
 }
