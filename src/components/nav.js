78e7a1c9db14abf687b3eed58a2849c931f5342b
daniel <dev.danielcw@gmail.com>
2024-02-08 12:36:10 -0600

    FEAT: added a nav element to the page

---
+++ src/components/nav.js
@@ -0,0 +1,47 @@
+
+
+
+const Nav = () => {
+    return (
+        <div className="navbar bg-base-100 fixed">
+            <div className="container m-auto">
+                    <div className="navbar-start flex lg:hidden">
+                        <div className="dropdown">
+                            <div tabIndex="0" role="button" className="btn btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </div>
+                            <ul tabIndex="0" className="bg-base-100 menu menu-lg dropdown-content">
+                                <li><a> Home </a></li>
+                                <li>
+                                    <a> Collections </a>
+                                    <ul>
+                                        <li><a> Favorites </a></li>
+                                        <li><a> Wishlist </a></li>
+                                        <li><a> Owned </a></li>
+                                    </ul>
+                                </li>
+                                <li><a> Browse </a></li>
+                                <li><a> Compare </a></li>
+
+                            </ul>
+                        </div>
+                        {/* mobile */}
+                    </div>
+                        
+                    <div className="hidden lg:flex ml-auto">
+                        {/* desktop */}
+                        <ul className="menu menu-horizontal px-1">
+                            <li><a> Home </a></li>
+
+                            <li><a> Browse </a></li>
+                            <li><a> Compare </a></li>
+                        </ul>   
+                    </div>                
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Nav
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,13 +2,16 @@ import React from "react";
 import Hero from "../components/hero";
 import books from '../images/books.jpg'
 import About from "../components/about";
+import Nav from "../components/nav";
 
 const Home = () => {
 
 
     return(
         <div className="bg-white">
+            <Nav />
             <Hero />
+            <About />
             {/* hero section */}
             {/* about section */}
             {/* new arrivals */}
