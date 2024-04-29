41e943b30b3bd08c1fd87016a0bd091d3222742b
daniel <dev.danielcw@gmail.com>
2024-04-29 10:27:32 -0500

    MINOR(hero): add searchbar and form element

--- src/components/about.js
+++ src/components/about.js
@@ -6,16 +6,12 @@ const About = () => {
 
                 <div className="mx-auto flex items-center max-w-max">
                     <div className="block prose lg:prose-lg p-8 lg:p-12">
-                        <h2> About </h2>
+                        <h2> How It Works </h2>
                         <p className="leading-normal text-neutral-content"> 
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                             when an unknown printer took a galley of type and scrambled it to make a type 
-                            specimen book. It has survived not only five centuries, but also the leap into 
-                            electronic typesetting, remaining essentially unchanged. It was popularised in 
-                            the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                            and more recently with desktop publishing software like Aldus PageMaker including 
-                            versions of Lorem Ipsum.
+                            specimen book.
                         </p>
                     </div>
                 </div>
