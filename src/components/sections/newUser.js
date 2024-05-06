e5e7f910fe1b2b6d68ee51d395151de329823345
daniel <dev.danielcw@gmail.com>
2024-05-06 17:21:48 -0500

    CHORE(style): fix styling for 'profile' and 'about' section

--- src/components/sections/newUser.js
+++ src/components/sections/newUser.js
@@ -3,14 +3,14 @@
 
 const NewUser = () => {
     return (
-        <div className="bg-base-200 min-h-half flex flex-col justify-center">
+        <div className="bg-base-300 min-h-half flex flex-col justify-center">
             <div className="flex container mx-auto p-8">
 
 
-                <div className="flex flex-col justify-center flex-grow">
-                    <div className="mx-auto text-left mb-16 block prose lg:prose-lg">
+                <div className="flex flex-col justify-center w-full sm:w-96 mx-auto">
+                    <div className="mx-auto text-center mb-16 block prose lg:prose-lg">
                         <h2 className="mb-4"> Ready to get started? </h2>
-                        <p> Sign up and start tracking today! </p> 
+                        <p> Sign up and take control of the way you eat today! </p> 
                     </div>                   
                     <form className="flex flex-col gap-2">
                         <input type="textarea" className="input" placeholder="First name" />
