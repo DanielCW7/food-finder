be83e0e86a9ba4b13dc0427e805defd0d1a141dc
daniel <dev.danielcw@gmail.com>
2024-05-06 12:43:55 -0500

    EDIT(stats): add example stat

--- src/components/sampleStats.js
+++ src/components/sampleStats.js
@@ -19,7 +19,7 @@ const SampleStats = () => {
                             </svg>
                         </div>
                         <div className="stat-title"> Calories consumed </div>
-                        <div className="stat-value text-info"> 367,789 </div>
+                        <div className="stat-value text-info"> +25,000 </div>
                         <div className="stat-desc"> this month </div>
                     </div>
 
@@ -34,8 +34,8 @@ const SampleStats = () => {
 
                     <div className="stat place-items-left border rounded-xl sm:w-max">
                         <div className="stat-figure text-info">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="inline-block w-12 h-12 stroke-current">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                             </svg>
                         </div>
                         <div className="stat-title"> Average plan price </div>
@@ -54,35 +54,17 @@ const SampleStats = () => {
                         <div className="stat-value text-info"> 302 </div>
                         <div className="stat-desc"> +10 this month </div>
                     </div>
-{/* 
-                    <div className="stat place-items-left">
-                        <div className="stat-title"> Meal plans made </div>
-                        <div className="stat-value"> 4 </div>
-                        <div className="stat-desc">  </div>
-                    </div>
-
-                    <div className="stat place-items-left">
-                        <div className="stat-title"> Fruit content </div>
-                        <div className="stat-value"> 17% </div>
-                    </div>
-
-                    <div className="stat place-items-left">
-                        <div className="stat-title "> Recipes bookmarked </div>
-                        <div className="stat-value"> 30 </div>
-                        <div className="stat-desc"> +10 this month </div>
-                    </div>
-
-                    <div className="stat place-items-left">
-                        <div className="stat-title"> Protein content </div>
-                        <div className="stat-value"> 34% </div>
-                        <div className="stat-desc">  </div>
-                    </div>
-
-                    <div className="stat place-items-left">
-                        <div className="stat-title"> Fiber content </div>
-                        <div className="stat-value"> 17% </div>
-                    </div> */}
-                    
+                    <div className="stat place-items-left border rounded-xl sm:w-max">
+                        
+                        <div className="stat-figure text-info">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="inline-block w-12 h-12 stroke-current">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                            </svg>
+                        </div>
+                        <div className="stat-title "> Plans made </div>
+                        <div className="stat-value text-info"> 5 </div>
+                        <div className="stat-desc"></div>
+                    </div> 
                 </div>
             </div>
         </div>
