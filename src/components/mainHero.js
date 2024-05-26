20f0651d5b4cffe7fd8747fc24617493a024d8f2
daniel <dev.danielcw@gmail.com>
2024-05-26 12:30:13 -0500

    CHORE(hero): add negative margin for main hero

--- src/components/mainHero.js
+++ src/components/mainHero.js
@@ -4,7 +4,7 @@ const MainHero = () => {
 
     return (
             <header className="container mx-auto">
-                <div className="flex justify-center items-center min-h-screen">
+                <div className="flex justify-center items-center min-h-screen -mt-10">
                     <div className="block prose lg:prose-lg p-8 lg:p-12">
                         <h1 className=""> 
                             <span className="text-primary-content">The Food Finder</span> helps you plan out every meal. 
