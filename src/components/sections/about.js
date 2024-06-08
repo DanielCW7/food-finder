3489f87e04da060a860c90227fa8178ae88d663d
Daniel <dev.danielcw@gmail.com>
2024-06-08 01:29:10 -0500

    CHORE(about): remove hovers for items tbd

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -57,7 +57,7 @@ const About = () => {
                         </div> 
                     </div>
 
-                    <div className="flex flex-col sm:flex-row transition-all rounded-xl p-8 sm:p-10 cursor-pointer hover:bg-base-300 hover:-translate-y-1">
+                    <div className="flex flex-col sm:flex-row transition-all rounded-xl p-8 sm:p-10 cursor-pointer ">
                         <div className="my-auto mr-6">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 sm:w-12 sm:h-12 stroke-accent">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
@@ -71,7 +71,7 @@ const About = () => {
                         </div> 
                     </div>
 
-                    <div className="flex flex-col sm:flex-row transition-all rounded-xl p-8 sm:p-10 cursor-pointer hover:bg-base-300 hover:-translate-y-1">
+                    <div className="flex flex-col sm:flex-row transition-all rounded-xl p-8 sm:p-10 cursor-pointer ">
                         <div className="my-auto mr-6">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 sm:w-12 sm:h-12 stroke-accent">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122" />
@@ -84,7 +84,7 @@ const About = () => {
                         </div> 
                     </div>
 
-                    <div className="flex flex-col sm:flex-row transition-all rounded-xl p-8 sm:p-10 cursor-pointer hover:bg-base-300 hover:-translate-y-1">
+                    <div className="flex flex-col sm:flex-row transition-all rounded-xl p-8 sm:p-10 cursor-pointer ">
                         <div className="my-auto mr-6">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 sm:w-12 sm:h-12 stroke-accent">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
