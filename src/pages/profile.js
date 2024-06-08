5a8dc8f42fb1ca81e138b816b86ea870138cf354
Daniel <dev.danielcw@gmail.com>
2024-06-07 23:14:51 -0500

    BUILD(profile): remove minor hero on profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,12 +1,10 @@
 import Nav from "../components/sections/nav";
 import Footer from "../components/footer";
-import MinorHero from "../components/minorHero";
 
 const Profile = () => {
     return (
         <>
             <Nav />
-            <MinorHero title="Browse" />
 
             <Footer />
         </>
