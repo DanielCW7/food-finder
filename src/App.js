f7a28cbcaebf3f3c930afc215910bafaa479d5fe
Daniel <dev.danielcw@gmail.com>
2025-05-14 11:49:18 -0500

    change from browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Home from './pages/home';
 import Browse from './pages/browse';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from 'react-router-dom';
 
 function App() {
 
   return (
 
-    <BrowserRouter>
+    <Router>
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
       <link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Lobster&display=swap" rel="stylesheet"></link>
@@ -15,7 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/browse" element={<Browse />} />
         </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
