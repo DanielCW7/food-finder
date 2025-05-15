3406755eada9b4d8fe0842bdb18f3c49f8b574c3
Daniel <dev.danielcw@gmail.com>
2025-05-15 06:59:40 -0500

    add backend

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const serverless = require("serverless-http")
+const { createClient } = require('@supabase/supabase-js');
+
+const app = express();
+
+// init supabase client
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// middleware
+app.use(express.json());
+const router = express.Router();
+
+// routes
+router.get('/browse', async (req, res) => {
+    try {
+        let { data, error } = await supabase.from('food_stats').select();
+        if (error) throw error;
+        res.status(200).json(data);
+    } catch(error) {
+        console.error("error fetching food", error.message);
+        res.status(500).json({ error: 'error fetching foods' })
+    } 
+});
+
+app.use('/', router)
+
+module.exports = serverless(app)
